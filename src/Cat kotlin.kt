3428754b5b2e1f73de09b5422d692dae49fa0bce
Awourrobberrt <sophierobberrt@gmail.com>
2021-05-18 17:59:24 +0300

    Cat kotlin

---
+++ src/Cat kotlin.kt
@@ -0,0 +1,35 @@
+fun main() {
+    println("crime")
+    println("Trevor")
+    println(250)
+    println("maximum")
+    println()
+
+}
+data class Book(var title:String,var author: String,var pages:Int){
+    fun ListBook(book:Book){
+        var TitleList= mutableListOf<Book>()
+        var AuthorList= mutableListOf<Book>()
+        var PageList= mutableListOf<Book>()
+    }
+}
+fun square(num:Int):Int{
+    for(num in  "square") {
+        return (300)
+
+
+    }
+
+    }
+class Truck(var regration:String,var totalWeight:Int,var capacity:Int){
+    fun Load(weight:Int){
+        var Vehicle=("KTYUF",250,72)
+        println(totalWeight)
+
+
+
+    }
+
+
+
+}
